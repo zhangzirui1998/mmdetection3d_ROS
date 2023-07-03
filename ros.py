3ce65449e3225bc9c082660dd920bf0e2bf46f57
dldd <58463987+cat722@users.noreply.github.com>
2023-07-03 15:52:42 +0800

    Update ros.py

--- ros.py
+++ ros.py
@@ -37,9 +37,7 @@ class Pointpillars_ROS:
     def lidar_callback(self, msg):
 
         # build the model from a config file and a checkpoint file
-        # 填写自己的实际路径：
-        # config_file = '/home/d/mmdet/src/mmdet/tools/configs/pointpillars/hv_pointpillars_secfpn_sbn-all_4x8_2x_nus-3d.py'
-        # checkpoint_file = '/home/d/mmdet/src/mmdet/tools/checkpoint/hv_pointpillars_secfpn_sbn-all_4x8_2x_nus-3d_20210826_225857-f19d00a3.pth'
+        # 填写自己的实际路径
         checkpoint_file = '/home/d/code/mmdetection3d_ros/mmdet/src/mmdet/tools/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class_20220301_150306-37dc2420.pth'
         config_file = '/home/d/code/mmdetection3d_ros/mmdet/src/mmdet/tools/configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class.py'
         model = init_model(config_file, checkpoint_file, device='cuda:0')
@@ -73,7 +71,6 @@ class Pointpillars_ROS:
         points = np.array(pc2.read_points_list(msg))
         points_class = get_points_type('LIDAR')
         points_mmdet3d = points_class(points, points_dim=points.shape[-1], attribute_dims=None)
-        # points_mmdet3d = points_class(points, points_dim=5, attribute_dims=None)
         torch.cuda.synchronize()
         start_time = time.perf_counter()
         result, data = inference_detector(model, points_mmdet3d)
@@ -85,35 +82,9 @@ class Pointpillars_ROS:
         scores = scores[mask]
         boxes_lidar = result[0]['boxes_3d'][mask].tensor.cpu().numpy()
         label = result[0]['labels_3d'][mask].numpy()
-        # boxes_lidar=result[0]['boxes_3d'].tensor.cpu().numpy()
-        # label=result[0]['labels_3d'].numpy()
-        # scores = result[0]['scores_3d'].numpy()
-        #print(points.size)
-        #print(data)
-        #print(points[0])
-        data = []
-        points1 = []
-        cats=["Pedestrian", "Cyclist", "Car"]
-        cat = ["m"]
         arr_bbox = BoundingBoxArray()
-        markerArray = MarkerArray()
         for i in range(scores.size):
             bbox = BoundingBox()
-            marker = Marker()
-            marker.header = msg.header
-            marker.type = marker.TEXT_VIEW_FACING
-            marker.id = int(label[i])
-            marker.text = f"{int(boxes_lidar[i][0])}{cat[0]} {cats[int(label[i])]}"
-            marker.action = marker.ADD
-            marker.frame_locked = True
-            marker.lifetime = rospy.Duration(1)
-            marker.scale.x, marker.scale.y,marker.scale.z = 0.8, 0.8, 0.8
-            marker.color.r, marker.color.g, marker.color.b, marker.color.a = 1.0, 0, 0, 1.0
-            marker.pose.position.x, marker.pose.position.y, marker.pose.position.z = boxes_lidar[i][0], boxes_lidar[i][1], boxes_lidar[i][2]+1
-            # boxes_lidar=result[i]['boxes_3d'].tensor.cpu().numpy()
-            # label=result[i]['labels_3d'].numpy()
-            # scores = result[i]['scores_3d'].numpy()
-
             bbox.header.frame_id = msg.header.frame_id
             bbox.header.stamp = rospy.Time.now()
             bbox.pose.position.x = float(boxes_lidar[i][0])
@@ -128,62 +99,13 @@ class Pointpillars_ROS:
             bbox.pose.orientation.z = q.z
             bbox.pose.orientation.w = q.w
             bbox.value = scores[i]
-            # bbox.value = 0.6
             bbox.label = label[i]
             # print(label[i])
             arr_bbox.boxes.append(bbox)
-            markerArray.markers.append(marker)
-            data.append(boxes_lidar[i][0])
-            data.append(boxes_lidar[i][1])
-            data.append(boxes_lidar[i][2])
-            data.append(boxes_lidar[i][3])
-            data.append(boxes_lidar[i][4])
-            data.append(boxes_lidar[i][5])
-            data.append(boxes_lidar[i][6])
-        #print(np.array(data).size)
-        #print(data)
-        #start_time1 = time.perf_counter()
-        #for j in range((int) (points.size/4)):
-          #  for k in range(scores.size):
-                #if (self.check_point_in_box(points[j], data[k + 0], data[k + 1], data[k + 2],
-                   #                    data[k + 3], data[k + 4], data[k + 5], data[k + 6])):
-                 #if (self.check_point_in_box(points[j], boxes_lidar[k][0], boxes_lidar[k][1], boxes_lidar[k][2],
-                             #       boxes_lidar[k][3], boxes_lidar[k][4], boxes_lidar[k][5], boxes_lidar[k][6]) == True):
-                    #break
-                 #else:
-                   # if (k == scores.size-1):
-                      #print(points[j])
-
-                      #points1.append(points[j])
-        #elapsed1 = time.perf_counter() - start_time
-        #print(f'pushtime: {elapsed1 * 1000:.1f} ms')
-        #print(points1.size)
         arr_bbox.header.frame_id = msg.header.frame_id
-        # arr_bbox.header.stamp = rospy.Time.now()
-        left_top = Float64MultiArray(data=data)
         if len(arr_bbox.boxes) is not 0:
             pub_bbox.publish(arr_bbox)
-            marker_pub.publish(markerArray)
-            pub_bboxtest.publish(left_top)
             self.publish_test(points, msg.header.frame_id)
-
-    def check_point_in_box(self,pts, box0, box1, box2, box3, box4, box5, box6):
-        """
-        pts[x,y,z]
-        box[c_x,c_y,c_z,dx,dy,dz,heading]
-    """
-
-        shift_x = pts[0] - box0
-        shift_y = pts[1] - box1
-        shift_z = pts[2] - box2
-        cos_a = math.cos(box6)
-        sin_a = math.sin(box6)
-        dx, dy, dz = box3, box4, box5
-        local_x = shift_x * cos_a + shift_y * sin_a;
-        local_y = shift_y * cos_a - shift_x * sin_a;
-        if ((abs(shift_z) > dz / 2.0) | (abs(local_x) > dx / 2.0) | (abs(local_y) > dy / 2.0)):
-            return False
-        return True
 
     def publish_test(self, cloud, frame_id):
         header = Header()
@@ -250,11 +172,8 @@ if __name__ == '__main__':
 
     # publisher
     pub_velo = rospy.Publisher("/modified", PointCloud2, queue_size=1)
-    pub_bboxtest = rospy.Publisher("/detections_track", Float64MultiArray, queue_size=10)
-    # pub_bboxtest = rospy.Publisher("/detections_track", detection, queue_size=10)
-
+    
     pub_bbox = rospy.Publisher("/detections", BoundingBoxArray, queue_size=10)
-    marker_pub = rospy.Publisher("/detections11", MarkerArray, queue_size=10)
 
     try:
         rospy.spin()

7809aabb581569ebe0835ad89164106f839e419b
everything <58463987+cat722@users.noreply.github.com>
2023-08-18 15:34:07 +0800

    Clean up parts of the code to make it more concise

--- ros.py
+++ ros.py
@@ -41,32 +41,7 @@ class Pointpillars_ROS:
         checkpoint_file = '/home/d/code/mmdetection3d_ros/mmdet/src/mmdet/tools/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class_20220301_150306-37dc2420.pth'
         config_file = '/home/d/code/mmdetection3d_ros/mmdet/src/mmdet/tools/configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class.py'
         model = init_model(config_file, checkpoint_file, device='cuda:0')
-
-        #pcl_msg = pc2.read_points(msg, field_names = ("x", "y", "z", "intensity"), skip_nans=True)
-        # 这里的field_names可以不要，不要就是把数据全部读取进来。也可以用field_names = ("x", "y", "z")这个只读xyz坐标
-        # 得到的pcl_msg是一个generator生成器，如果要一次获得全部的点，需要转成list
-        #np_p = np.array(list(pcl_msg), dtype=np.float32)
-        # print(np_p.shape)
-        # 旋转轴
-        # rand_axis = [0,1,0]
-        # 旋转角度
-        # yaw = 0.1047
-        # yaw = 0.0
-        # 返回旋转矩阵
-        # rot_matrix = self.rotate_mat(rand_axis, yaw)
-        # np_p_rot = np.dot(rot_matrix, np_p[:,:3].T).T
-
-        # convert to xyzi point cloud
-        # x = np_p[:, 0].reshape(-1)
-        # print(np.max(x),np.min(x))
-        # y = np_p[:, 1].reshape(-1)
-        # z = np_p[:, 2].reshape(-1)
-        # if np_p.shape[1] == 4: # if intensity field exists
-        #    i = np_p[:, 3].reshape(-1)
-        # else:
-        #   i = np.zeros((np_p.shape[0], 1)).reshape(-1)
-        # points = np.stack((x, y, z, i)).T
-
+        
         # 在回调函数中
         points = np.array(pc2.read_points_list(msg, field_names = ("x", "y", "z","intensity"), skip_nans=True))
         points[:, [3]] = 0
@@ -121,44 +96,6 @@ class Pointpillars_ROS:
 
         pub_velo.publish(msg_segment)
         # pub_image.publish(image_msg)
-
-
-def _make_point_field(num_field):
-    msg_pf1 = pc2.PointField()
-    msg_pf1.name = np.str_('x')
-    msg_pf1.offset = np.uint32(0)
-    msg_pf1.datatype = np.uint8(7)
-    msg_pf1.count = np.uint32(1)
-
-    msg_pf2 = pc2.PointField()
-    msg_pf2.name = np.str_('y')
-    msg_pf2.offset = np.uint32(4)
-    msg_pf2.datatype = np.uint8(7)
-    msg_pf2.count = np.uint32(1)
-
-    msg_pf3 = pc2.PointField()
-    msg_pf3.name = np.str_('z')
-    msg_pf3.offset = np.uint32(8)
-    msg_pf3.datatype = np.uint8(7)
-    msg_pf3.count = np.uint32(1)
-
-    msg_pf4 = pc2.PointField()
-    msg_pf4.name = np.str_('intensity')
-    msg_pf4.offset = np.uint32(16)
-    msg_pf4.datatype = np.uint8(7)
-    msg_pf4.count = np.uint32(1)
-
-    if num_field == 4:
-        return [msg_pf1, msg_pf2, msg_pf3, msg_pf4]
-
-    msg_pf5 = pc2.PointField()
-    msg_pf5.name = np.str_('label')
-    msg_pf5.offset = np.uint32(20)
-    msg_pf5.datatype = np.uint8(4)
-    msg_pf5.count = np.uint32(1)
-
-    return [msg_pf1, msg_pf2, msg_pf3, msg_pf4, msg_pf5]
-
 
 if __name__ == '__main__':
     global sec
